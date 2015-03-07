c55b54ebada7e9e2237b3b8c63760808322fcf4d
DraperStudio <hello@draperstud.io>
2015-03-07 12:01:27 +0100

    Update comments

--- src/SpotifyExtendSocialite.php
+++ src/SpotifyExtendSocialite.php
@@ -6,7 +6,9 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 class SpotifyExtendSocialite
 {
     /**
-     * Execute the provider.
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
      */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
